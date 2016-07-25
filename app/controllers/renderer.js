050b8e181fdfcf28e4569d537653e6a085f677dc
SkyzohKey <skyzohkey@protonmail.com>
2016-07-25 17:44:32 +0200

    separate code

--- app/controllers/renderer.js
+++ app/controllers/renderer.js
@@ -9,29 +9,9 @@ const emojione = require('emojione');
 const dateformat = require('date-format');
 
 const ToxStatus = require('./ToxStatus.js');
+const stringHelper = require('../helpers/string.js');
 
 console.log("renderer", __dirname);
-
-/**
-* HTML escaping.
-**/
-String.prototype.escape = function() {
-  var tagsToReplace = {
-    '&': '&amp;',
-    '<': '&lt;',
-    '>': '&gt;'
-  };
-  return this.replace(/[&<>]/g, function(tag) {
-    return tagsToReplace[tag] || tag;
-  });
-};
-/**
-* Newline (\r\n) to <br> or <br/> if xhtml.
-**/
-String.prototype.nl2br = function (is_xhtml) {
-  var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br ' + '/>' : '<br>';
-  return this.replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
-};
 
 /**
 * Our Tox class.
